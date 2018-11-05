87f11282084979ec30e0340f7677626e92a7430e
Janusz79 <janusz79@gmail.com>
2018-11-05 20:06:53 +0100

    set up rollup

---
+++ src/grimm-user-pic.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'GrimmUserPic', // vue component name
+    data() {
+        return {
+            counter: 5,
+            initCounter: 5,
+        };
+    },
+}
+</script>
+
+<template>
+    <div class="grimm-user-pic">
+        <p>The counter is set to <b>{{counter}}</b>.</p>
+        <button @click="counter += 1">Click +1</button>
+        <button @click="counter -= 1">Click -1</button>
+        <button @click="counter = initCounter">Reset</button>
+    </div>
+</template>
+
+<style scoped>
+    .grimm-user-pic {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+        border: 1px solid #ccc;
+        background: #eaeaea;
+        text-align: center;
+        padding: 25px;
+    }
+    .grimm-user-pic p {
+        margin: 0 0 1em;
+    }
+</style>
